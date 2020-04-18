2055c6f127a1ba3d64170526fadf06dba64e6277
Damian Sobczak <damiansobczak@MacBook-Pro-Damian.local>
2020-04-18 11:07:42 +0200

    Unit test for favorites component

--- src/components/Favorite/Favorite.test.tsx
+++ src/components/Favorite/Favorite.test.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, render } from "enzyme";
 import renderer from "react-test-renderer";
 import Favorite from "./Favorite";
 import EmptyFavorite from "./EmptyFavorite";
 import { ThemeContext } from "../ThemeContext";
+import { BrowserRouter } from "react-router-dom";
 
 const context = {
     isOpen: false,
@@ -24,12 +25,50 @@ const context = {
 const context2 = {
     isOpen: false,
     isFavorite: () => false,
-    favorites: null
+    favorites: []
 }
 
 describe("<Favorite />", () => {
-    it("renders component correctly", () => {
-        const tree = renderer.create(<ThemeContext.Provider value={context}><Favorite /></ThemeContext.Provider>).toJSON();
+    it("renders component correctly when selected", () => {
+        const tree = renderer.create(
+            <ThemeContext.Provider value={context}>
+                <BrowserRouter>
+                    <Favorite />
+                </BrowserRouter>
+            </ThemeContext.Provider>).toJSON();
         expect(tree).toMatchSnapshot();
-    })
+    });
+
+    it("renders component correctly when empty", () => {
+        const tree = renderer.create(
+            <ThemeContext.Provider value={context2}>
+                <BrowserRouter>
+                    <Favorite />
+                </BrowserRouter>
+            </ThemeContext.Provider>).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+    it("shows correctly favorite item in modal", () => {
+        const wrapper = render(
+            <ThemeContext.Provider value={context}>
+                <BrowserRouter>
+                    <Favorite />
+                </BrowserRouter>
+            </ThemeContext.Provider>
+        );
+        expect(wrapper.find(".offer__title").html()).toContain("Osiedle Babina, ul. Babina 20/3A");
+        expect(wrapper.find(".offer__media > img").prop("src")).toContain("http://localhost:8888/estate/wp-content/uploads/2020/01/image-1.jpg");
+    });
+
+    it("shows information when empty favorites", () => {
+        const wrapper = render(
+            <ThemeContext.Provider value={context2}>
+                <BrowserRouter>
+                    <Favorite />
+                </BrowserRouter>
+            </ThemeContext.Provider>
+        );
+        expect(wrapper.find(".favorite__empty").html()).toContain("Aktualnie nie masz");
+    });
 });
